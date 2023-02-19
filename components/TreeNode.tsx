f133a6c25b8f08f3709deb31d497cf4ab7b64788
Disorder AA <disorrder@gmail.com>
2023-02-19 16:42:43 +0100

    optimize scroll

--- components/TreeNode.tsx
+++ components/TreeNode.tsx
@@ -20,7 +20,18 @@ export default function TreeNode({ value, level }: Props) {
   const className = cn(
     "TreeNode",
     "flex items-center gap-2 px-4 py-2 cursor-pointer transition-colors",
-    `pl-${level * 4}`,
+    // `pl-${level * 4}`, // Idk why Tailwind can't handle it
+    {
+      "pl-8": level === 2,
+      "pl-12": level === 3,
+      "pl-16": level === 4,
+      "pl-20": level === 5,
+      "pl-24": level === 6,
+      "pl-28": level === 7,
+      "pl-32": level === 8,
+      "pl-36": level === 9,
+      "pl-40": level === 10,
+    },
     "hover:bg-sky-50",
     selected?.key === value.key && "bg-sky-100"
   );
--- components/TreeView.tsx
+++ components/TreeView.tsx
@@ -1,7 +1,12 @@
+import { UIEvent } from "react";
 import { Skeleton } from "@mui/material";
+import cn from "classnames";
+
 import { IStat } from "../utils/stats.type";
 import { Dictionary, TrailNode } from "../utils/transform";
 import TreeNode from "./TreeNode";
+
+import styles from "./TreeView.module.css";
 
 interface Props {
   data: Dictionary<TrailNode<IStat>>;
@@ -18,9 +23,52 @@ export default function TreeView({ data }: Props) {
     );
   }
 
+  const className = cn(
+    "TreeView w-full overflow-y-auto rounded-md",
+    styles.TreeView
+  );
+
+  function handleScroll(e: UIEvent<HTMLDivElement>) {
+    const target = e.target as HTMLElement;
+    const { scrollTop, scrollHeight, clientHeight, children } = target;
+
+    // Hide invisible nodes to improve performance
+    // Guess all nodes have the same height
+
+    const visibleGap = 3;
+
+    const avgElementHeight = scrollHeight / children.length;
+    const firstVisibleIndex = Math.max(
+      0,
+      Math.floor(scrollTop / avgElementHeight) - visibleGap
+    );
+    const lastVisibleIndex = Math.min(
+      children.length - 1,
+      Math.floor((scrollTop + clientHeight) / avgElementHeight) + visibleGap
+    );
+
+    target.style.setProperty(
+      "--gap-before",
+      `${firstVisibleIndex * avgElementHeight}px`
+    );
+    target.style.setProperty(
+      "--gap-after",
+      `${(children.length - 1 - lastVisibleIndex) * avgElementHeight}px`
+    );
+
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i] as HTMLElement;
+      if (i < firstVisibleIndex || i > lastVisibleIndex) {
+        child.style.display = "none";
+      } else {
+        child.style.display = "";
+      }
+    }
+  }
+
   return (
-    <div className="TreeView w-full overflow-y-auto rounded-md">
-      {data.map((node) => (
+    <div className={className} onScroll={handleScroll}>
+      {data.map((node, i) => (
         <TreeNode value={node} level={1} key={node.key} />
       ))}
     </div>
